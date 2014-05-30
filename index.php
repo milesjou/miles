50f1493aa69673a075318236b4daa96c06c9bf14
milesjou <lawrencejou@gmail.com>
2014-05-30 16:52:28 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+    </head>
+
+<body>
+
+    <form name="form1" method="post" action="bmi_process.php">
+        BMI計算<br/>
+        身高: <input type="text" name="height" size="30"/>cm<br/>
+        體重: <input type="weight" name="weight" size="30"/>kg<br/>
+        <input type="submit" value="送出計算">
+        <input type="reset" value="重設">
+    </form>
+</body>
+
+</html>
